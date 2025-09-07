fbe82593cecb0293c3658b6746b25d882b399160
easkae1337 <easkae1337@gmail.com>
2025-09-07 08:39:51 +0000

    Add sources to test

---
+++ testproj/src/level_1/nested.cpp
@@ -0,0 +1,9 @@
+#include "nested.h"
+
+void nested()
+{
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cout << "nested foo: " << i << std::endl;
+    }
+}
--- testproj/src/test1.cpp
+++ testproj/src/test1.cpp
@@ -1,8 +1,10 @@
 #include "test12f.h"
+#include "level_1/nested.h"
 
 int main()
 {
     test2foo();
+    nested();
 
     return 0;
 }
